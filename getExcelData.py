c6231b68cdbde9dd42de3e7b311a725f156eed39
matiasandrade <matias.andrade.ca@gmail.com>
2016-12-09 00:39:40 -0500

    Updated getExcelData

--- getExcelData.py
+++ getExcelData.py
@@ -7,24 +7,21 @@ hour = minute*60
 day = hour*24
 month = day*30
 
-start = float(time.time()) - (90*day)
-end = time.time()
+defStart = float(time.time()) - (90*day)
+defEnd = time.time()
+	polo = poloniex.Poloniex(extend=True)
+	btcData = polo.marketChart(pair, day, start, end)
+	print(btcData[0])
+	outputList = []
+	for sample in btcData:
+		outStr = sample['high'], sample['low'], sample['open'], sample['close'], sample['volume']
 
-polo = poloniex.Poloniex(extend=True)
+		outputList.append(outStr)
 
-btcData = polo.marketChart('USDT_BTC', day, start, end)
-print(btcData[0])
-outputList = []
-for sample in btcData:
-	outStr = sample['high'], sample['low'], sample['open'], sample['close'], sample['volume']
-
-	outputList.append(outStr)
-
-with open("C:/Users/mat97/Desktop/exampleExcel.csv", 'w') as f:
-	for stringlet in outputList:
-		print(str(stringlet))
-		outStringlet = str(stringlet).replace("(", "")
-		outStringlet = outStringlet.replace("'", "")
-		outStringlet = outStringlet.replace(")", "")
-		f.write(outStringlet)
-		f.write("\n")
+		for stringlet in outputList:
+			print(str(stringlet))
+			outStringlet = str(stringlet).replace("(", "")
+			outStringlet = outStringlet.replace("'", "")
+			outStringlet = outStringlet.replace(")", "")
+			f.write(outStringlet)
+			f.write("\n")
